68bd488f2285967ca87516116740b3ecf06a8d14
nix18 <kyj20167@gmail.com>
2021-07-15 16:46:36 +0800

    支持多用户

---
+++ user.py
@@ -0,0 +1,9 @@
+class user():
+    cookie_str = ""
+    user_page = ""
+    plus_key = ""
+
+    def __init__(self, cookie_str, user_page, plus_key):
+        self.cookie_str = cookie_str
+        self.user_page = user_page
+        self.plus_key = plus_key
